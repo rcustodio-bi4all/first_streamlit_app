6b57d7b86b50a08ab643ac6c6666a1bd72453577
rcustodio-bi4all <62931044+rcustodio-bi4all@users.noreply.github.com>
2022-09-30 12:10:44 +0100

    Update streamlit_app.py
    Added breakfast menu with typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My Parents New Health Diner")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Lake, Spinach & Rocket Smoothie')
+streamit.text('Hard-Boiled Free-Range Egg')
